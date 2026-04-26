4c0a3c603631da5b530eda3bc5f0694c70e610b4
Hadrian Sabino <hadrian.sabino@gmail.com>
2026-04-25 18:48:43 -0700

    first comment

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [helloMessage, setMessage] = useState("World");
+  const [showPeanutImage, setShowPeanutImage] = useState(false);
+  const [changeButton, setButtonName] = useState("CHANGE");
+
+  const[bgColor, setBgColor] = useState("white");
+
+  const handleChange = () => {
+    setMessage("Crap Nut"); // displays peanut image when button pressed
+    setShowPeanutImage(true); // set true to show image
+    setButtonName("RESET"); // shows "RESET" on button
+    setBgColor("rainbow-in-action"); // starts rainbow background animation
+  };
+
+  const handleReset = () => {
+    setMessage("World"); // displays back to original message
+    setShowPeanutImage(false); // hide it when 'RESET' button pressed
+    setButtonName("CHANGE"); // shows "CHANGE" on button
+    setBgColor("white");
+  };
+
+  return (
+
+  <div className={bgColor} style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello {helloMessage}!</h1>
+
+      <button
+        onClick={changeButton == "CHANGE" ? handleChange : handleReset} // based on current name of button
+        style={{
+          border: "5px inset black",
+          padding: "10px 20px",
+          marginTop: "15px",
+          fontSize: "16px",
+          cursor: "pointer",
+        }}
+      >
+        {changeButton}
+      </button>
+
+      {showPeanutImage && (
+        <div style={{ justifyContent: "center", marginTop: "20px" }}>
+          <img
+            src="/peanut.png"
+            alt="Peanut aka Crap Nut"
+            width={200}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App
