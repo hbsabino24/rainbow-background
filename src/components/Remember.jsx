dac35f6ae06e7412ca152cfcfcd041045698a23e
hbsabino24 <hadrian.sabino@gmail.com>
2026-08-17 20:02:58 -0700

    useRef updated complete

--- src/components/Remember.jsx
+++ src/components/Remember.jsx
@@ -12,22 +12,59 @@ import React, { useState, useEffect, useRef } from 'react';
 
 function Remember() {
 
-    let [number, setNumber] = useState(0);
+    // let [number, setNumber] = useState(0);
+
+    // const ref = useRef("pizza");
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const inputRef3 = useRef(null);
+
+    // console.log(ref);
 
     useEffect(() => {
         console.log("COMPONENT RENDERED")
+        // console.log(inputRef);
     });
 
-    function handleClick() {
-        setNumber(n => n + 1);
-        console.log('clicked, updated number: ' + number);
+    function handleClick1() {
+        // setNumber(n => n + 1);
+        // ref.current++;
+        inputRef1.current.focus();
+        inputRef1.current.style.backgroundColor = "yellow";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "";
+    }
+    
+    function handleClick2() {
+        inputRef2.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "yellow";
+        inputRef3.current.style.backgroundColor = "";
+    }
+    
+    function handleClick3() {
+        inputRef3.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "yellow";
     }
 
     return (
         <div>
-            <button onClick={handleClick}>
-                Click me!
+            <button onClick={handleClick1}>
+                Click me 1!
             </button>
+            <input ref={inputRef1} />            
+            
+            <button onClick={handleClick2}>
+                Click me 2!
+            </button>
+            <input ref={inputRef2} />            
+            
+            <button onClick={handleClick3}>
+                Click me 3!
+            </button>
+            <input ref={inputRef3} />
         </div>
     )
 }
